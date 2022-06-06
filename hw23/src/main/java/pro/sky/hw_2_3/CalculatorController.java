4b1c07bd479f89128e7d06fab3e5cce006e19e0a
ArtemCH240322 <sun2383@mail.ru>
2022-06-06 18:37:24 +0300

    Первый комит. Создал пакет. Запрос создал, запрос вернулся. В вел значения колькулятора. Ошибка читаемая. Задание выполнил.

--- hw23/src/main/java/pro/sky/hw_2_3/CalculatorController.java
+++ hw23/src/main/java/pro/sky/hw_2_3/CalculatorController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import pro.sky.hw_2_3.service.CalculatorServuce;
 
 import java.util.Objects;
 
@@ -11,10 +12,15 @@ import java.util.Objects;
 @RequestMapping("/calculator")
 public class CalculatorController {
 
+private CalculatorServuce calculatorServuce;
+
+    public CalculatorController(CalculatorServuce calculatorServuce) {
+        this.calculatorServuce = calculatorServuce;
+    }
 
     @GetMapping
     public String greetings() {
-        return "Добро пожаловать в калькулятор";
+        return calculatorServuce.greetings();
     }
 
     @GetMapping("/plus")
@@ -23,7 +29,7 @@ public class CalculatorController {
         if (Objects.isNull(a) || Objects.isNull(b)){
             return "Не правельно переданны параметры";
         }
-        return a + " + " + b + " = " + (a + b);
+        return buildString(a, b, calculatorServuce.plus(a, b), "+");
     }
 
     @GetMapping("/minus")
@@ -32,7 +38,7 @@ public class CalculatorController {
         if (Objects.isNull(a) || Objects.isNull(b)){
             return "Не правельно переданны параметры";
         }
-        return a + " - " + b + " = " + (a - b);
+        return buildString(a, b, calculatorServuce.minus(a, b), "-");
     }
 
     @GetMapping("/multiply")
@@ -41,21 +47,30 @@ public class CalculatorController {
         if (Objects.isNull(a) || Objects.isNull(b)){
             return "Не правельно переданны параметры";
         }
-        return a + " * " + b + " = " + (a * b);
+        return buildString(a, b, calculatorServuce.multioly(a, b), "*");
     }
 
     @GetMapping("/divide")
-            public String divide(@RequestParam (value = "numb1", required = false) Float a,
-                                 @RequestParam (value = "numb2", required = false) Float b){
-                if (Objects.isNull(a) || Objects.isNull(b)) {
-                    return "Не правельно переданны параметры";
-                }
+    public String divide(@RequestParam (value = "numb1", required = false) Float a,
+                         @RequestParam (value = "numb2", required = false) Float b){
+        if (Objects.isNull(a) || Objects.isNull(b)) {
+            return "Не правельно переданны параметры";
+        }
 
-                if (b == 0) {
-                    return "На ноль делить нельзя!";
-                }
+        if (b == 0) {
+            return "На ноль делить нельзя!";
+        }
 
-                return a + " / " + b + " = " + (a / b);
-                    }
+        return buildString(a, b, calculatorServuce.divide(a, b), "/");
+    }
 
-                }
+    private String buildString(float a,
+                               float b,
+                               float result,
+                               String operation) {
+        return String.format("%.f1 %s %.f1 = %.f1", a, operation, b, result);
+
+    }
+
+
+}
